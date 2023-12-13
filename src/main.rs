d22b64067cf91330f64176e3a69d9a1a10c320cb
faqsarg <fsargiottodev@gmail.com>
2023-12-12 23:10:29 -0300

    add connection handler. read data with buffer

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("connection established");
+
+        handle_conn(stream);
     }
+}
+
+fn handle_conn(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_req: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 }
