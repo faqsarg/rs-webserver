b2e43fd0cda8c770fce2ff91e32b9a3ee04bb5fa
faqsarg <fsargiottodev@gmail.com>
2023-12-17 20:12:32 -0300

    t

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ fn main() {
     });
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
 
+    println!("listening on port 8000");
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
